992248add0f089a79bf723d168b1504cf62dc8fa
student <student@ru>
2026-01-20 17:45:25 +0300

    added sprites to start view, moved buttons to images folder

--- base.py
+++ base.py
@@ -14,6 +14,11 @@ class StartView(arcade.View):
     TOMATO_SPEED = 100
 
     BUTTON_OFFSET = 10
+    SPRITES_PATHS = [
+        'images/beet.png',
+        'images/carrot.png',
+        'images/potato.png'
+    ]
 
     def __init__(self, game_view):
         super().__init__()
@@ -26,7 +31,7 @@ class StartView(arcade.View):
         self.batch = Batch()
 
         for _ in range(self.TOMATO_COUNT):
-            tomato = arcade.Sprite('images/tomato.png', scale=0.1)
+            tomato = arcade.Sprite(random.choice(self.SPRITES_PATHS), scale=2)
 
             tomato.left = random.randint(0, int(self.window.width - tomato.width))
             tomato.bottom = random.randint(0, int(self.window.height - tomato.height))
@@ -41,32 +46,46 @@ class StartView(arcade.View):
 
             self.tomatoes.append(tomato)
 
-        self.start_game_text = arcade.Text('Начать игру', self.window.width / 2, self.window.height / 8 * 4,
+        self.button_texture = arcade.load_texture('images/button_green.png')
+
+        self.start_game_text = arcade.Text('Начать игру', self.window.width / 2, self.window.height / 8 * 5,
                                            batch=self.batch,
                                            anchor_x='center', anchor_y='center', font_size=17,
                                            font_name='Press Start 2P')
         self.start_game_rect = arcade.rect.XYWH(self.window.width / 2,
-                                                self.window.height / 2,
+                                                self.window.height / 8 * 5,
                                                 300, 100)
-        self.start_game_color = arcade.color.YELLOW_ORANGE
+        self.start_game_color = arcade.color.WHITE
+
+        self.title_game_text = arcade.Text('Титры', self.window.width / 2,
+                                           self.window.height / 8 * 4 - self.BUTTON_OFFSET,
+                                           batch=self.batch,
+                                           anchor_x='center', anchor_y='center', font_size=17,
+                                           font_name='Press Start 2P')
+        self.title_game_rect = arcade.rect.XYWH(self.window.width / 2,
+                                                self.window.height / 8 * 4 - self.BUTTON_OFFSET,
+                                                300, 100)
+        self.title_game_color = arcade.color.WHITE
 
         self.close_game_text = arcade.Text('Выйти', self.window.width / 2,
-                                           self.window.height / 8 * 3 - self.BUTTON_OFFSET,
+                                           self.window.height / 8 * 3 - self.BUTTON_OFFSET * 2,
                                            batch=self.batch,
                                            anchor_x='center', anchor_y='center', font_size=17,
                                            font_name='Press Start 2P')
         self.close_game_rect = arcade.rect.XYWH(self.window.width / 2,
-                                                self.window.height / 8 * 3 - self.BUTTON_OFFSET,
+                                                self.window.height / 8 * 3 - self.BUTTON_OFFSET * 2,
                                                 300, 100)
-        self.close_game_color = arcade.color.YELLOW_ORANGE
+        self.close_game_color = arcade.color.WHITE
 
     def on_draw(self):
         self.clear()
         self.tomatoes.draw()
-        arcade.draw.draw_rect_filled(self.start_game_rect, self.start_game_color)
-        arcade.draw.draw_rect_outline(self.start_game_rect, arcade.color.BLACK, border_width=3)
-        arcade.draw.draw_rect_filled(self.close_game_rect, self.close_game_color)
-        arcade.draw.draw_rect_outline(self.close_game_rect, arcade.color.BLACK, border_width=3)
+        self.start_game_text.color = self.start_game_color
+        self.close_game_text.color = self.close_game_color
+        self.title_game_text.color = self.title_game_color
+        arcade.draw_texture_rect(self.button_texture, self.start_game_rect)
+        arcade.draw_texture_rect(self.button_texture, self.close_game_rect)
+        arcade.draw_texture_rect(self.button_texture, self.title_game_rect)
         self.batch.draw()
 
     def on_update(self, delta_time: float):
@@ -99,15 +118,25 @@ class StartView(arcade.View):
                 self.close_game_rect.bottom <= y <= self.close_game_rect.top:
             self.window.close()
 
+        if self.title_game_rect.left <= x <= self.title_game_rect.right and \
+                self.title_game_rect.bottom <= y <= self.title_game_rect.top:
+            print('ТИТРЫ')
+
     def on_mouse_motion(self, x: int, y: int, dx: int, dy: int) -> bool | None:
         if self.start_game_rect.left <= x <= self.start_game_rect.right and \
                 self.start_game_rect.bottom <= y <= self.start_game_rect.top:
-            self.start_game_color = arcade.color.ORANGE_RED
-        else:
             self.start_game_color = arcade.color.YELLOW_ORANGE
+        else:
+            self.start_game_color = arcade.color.WHITE
 
         if self.close_game_rect.left <= x <= self.close_game_rect.right and \
                 self.close_game_rect.bottom <= y <= self.close_game_rect.top:
-            self.close_game_color = arcade.color.ORANGE_RED
-        else:
             self.close_game_color = arcade.color.YELLOW_ORANGE
+        else:
+            self.close_game_color = arcade.color.WHITE
+
+        if self.title_game_rect.left <= x <= self.title_game_rect.right and \
+                self.title_game_rect.bottom <= y <= self.title_game_rect.top:
+            self.title_game_color = arcade.color.YELLOW_ORANGE
+        else:
+            self.title_game_color = arcade.color.WHITE
