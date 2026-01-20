aecf7bc932051e3f7ac55ca3a845dc117bc6379c
dexon401 <@>
2026-01-20 18:52:47 +0300

    added difficulty buttons to start view

--- base.py
+++ base.py
@@ -7,6 +7,7 @@ from pyglet.graphics import Batch
 class Window(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title, resizable=False)
+        self.coins = 0
 
 
 class StartView(arcade.View):
@@ -24,10 +25,12 @@ class StartView(arcade.View):
         super().__init__()
 
         self.game_view = game_view
+        self.level_select_mode = False
 
     def setup(self):
         self.background_color = arcade.color.TEA_GREEN
-        self.tomatoes = arcade.SpriteList()
+
+        self.vegetables = arcade.SpriteList()
         self.batch = Batch()
 
         for _ in range(self.TOMATO_COUNT):
@@ -44,13 +47,13 @@ class StartView(arcade.View):
             while abs(tomato.speed_x) < 15:
                 tomato.speed_x = random.randint(-self.TOMATO_SPEED, self.TOMATO_SPEED)
 
-            self.tomatoes.append(tomato)
+            self.vegetables.append(tomato)
 
         self.button_texture = arcade.load_texture('images/button_green.png')
 
         self.start_game_text = arcade.Text('Начать игру', self.window.width / 2, self.window.height / 8 * 5,
                                            batch=self.batch,
-                                           anchor_x='center', anchor_y='center', font_size=17,
+                                           anchor_x='center', anchor_y='center', font_size=12,
                                            font_name='Press Start 2P')
         self.start_game_rect = arcade.rect.XYWH(self.window.width / 2,
                                                 self.window.height / 8 * 5,
@@ -60,7 +63,7 @@ class StartView(arcade.View):
         self.title_game_text = arcade.Text('Титры', self.window.width / 2,
                                            self.window.height / 8 * 4 - self.BUTTON_OFFSET,
                                            batch=self.batch,
-                                           anchor_x='center', anchor_y='center', font_size=17,
+                                           anchor_x='center', anchor_y='center', font_size=12,
                                            font_name='Press Start 2P')
         self.title_game_rect = arcade.rect.XYWH(self.window.width / 2,
                                                 self.window.height / 8 * 4 - self.BUTTON_OFFSET,
@@ -70,26 +73,54 @@ class StartView(arcade.View):
         self.close_game_text = arcade.Text('Выйти', self.window.width / 2,
                                            self.window.height / 8 * 3 - self.BUTTON_OFFSET * 2,
                                            batch=self.batch,
-                                           anchor_x='center', anchor_y='center', font_size=17,
+                                           anchor_x='center', anchor_y='center', font_size=12,
                                            font_name='Press Start 2P')
         self.close_game_rect = arcade.rect.XYWH(self.window.width / 2,
                                                 self.window.height / 8 * 3 - self.BUTTON_OFFSET * 2,
                                                 300, 100)
         self.close_game_color = arcade.color.WHITE
 
+        self.free_game_text = arcade.Text('', self.window.width / 2,
+                                          self.window.height / 8 * 2 - self.BUTTON_OFFSET * 3,
+                                          batch=self.batch,
+                                          anchor_x='center', anchor_y='center', font_size=12,
+                                          font_name='Press Start 2P')
+        self.free_game_rect = arcade.rect.XYWH(self.window.width / 2,
+                                               self.window.height / 8 * 2 - self.BUTTON_OFFSET * 3,
+                                               300, 100)
+        self.free_game_color = arcade.color.WHITE
+
+        self.back_game_text = arcade.Text('', 125, 725, batch=self.batch, anchor_x='center', anchor_y='center',
+                                          font_size=12, font_name='Press Start 2P')
+        self.back_game_rect = arcade.rect.LBWH(25, self.window.height - 125, 200, 100)
+        self.back_game_color = arcade.color.WHITE
+
     def on_draw(self):
         self.clear()
-        self.tomatoes.draw()
+        self.vegetables.draw()
+
+        self.start_game_text.text = 'Начать игру' if not self.level_select_mode else 'Легкий'
+        self.close_game_text.text = 'Выйти' if not self.level_select_mode else 'Сложный'
+        self.title_game_text.text = 'Титры' if not self.level_select_mode else 'Продвинутый'
+        self.free_game_text.text = '' if not self.level_select_mode else 'Свободная игра'
+        self.back_game_text.text = '' if not self.level_select_mode else 'Назад'
+
         self.start_game_text.color = self.start_game_color
         self.close_game_text.color = self.close_game_color
         self.title_game_text.color = self.title_game_color
+        self.free_game_text.color = self.free_game_color
+        self.back_game_text.color = self.back_game_color
+
         arcade.draw_texture_rect(self.button_texture, self.start_game_rect)
         arcade.draw_texture_rect(self.button_texture, self.close_game_rect)
         arcade.draw_texture_rect(self.button_texture, self.title_game_rect)
+        if self.level_select_mode:
+            arcade.draw_texture_rect(self.button_texture, self.free_game_rect)
+            arcade.draw_texture_rect(self.button_texture, self.back_game_rect)
         self.batch.draw()
 
     def on_update(self, delta_time: float):
-        for tomato in self.tomatoes:
+        for tomato in self.vegetables:
             tomato.center_x += tomato.speed_x * delta_time
             tomato.center_y += tomato.speed_y * delta_time
 
@@ -112,15 +143,40 @@ class StartView(arcade.View):
     def on_mouse_press(self, x: int, y: int, button: int, modifiers: int) -> bool | None:
         if self.start_game_rect.left <= x <= self.start_game_rect.right and \
                 self.start_game_rect.bottom <= y <= self.start_game_rect.top:
-            self.window.show_view(self.game_view)
+            if not self.level_select_mode:
+                self.level_select_mode = True
+            else:
+                self.window.show_view(self.game_view)
+                self.level_select_mode = False
 
         if self.close_game_rect.left <= x <= self.close_game_rect.right and \
                 self.close_game_rect.bottom <= y <= self.close_game_rect.top:
-            self.window.close()
+            if not self.level_select_mode:
+                self.window.close()
+            else:
+                self.window.show_view(self.game_view)
+                self.level_select_mode = False
 
         if self.title_game_rect.left <= x <= self.title_game_rect.right and \
                 self.title_game_rect.bottom <= y <= self.title_game_rect.top:
-            print('ТИТРЫ')
+            if not self.level_select_mode:
+                print('ТИТРЫ')
+            else:
+                self.window.show_view(self.game_view)
+                self.level_select_mode = False
+
+        if self.free_game_rect.left <= x <= self.free_game_rect.right and \
+                self.free_game_rect.bottom <= y <= self.free_game_rect.top:
+            if not self.level_select_mode:
+                return
+            else:
+                self.window.show_view(self.game_view)
+                self.level_select_mode = False
+
+        if self.back_game_rect.left <= x <= self.back_game_rect.right and \
+                self.back_game_rect.bottom <= y <= self.back_game_rect.top:
+            if self.level_select_mode:
+                self.level_select_mode = False
 
     def on_mouse_motion(self, x: int, y: int, dx: int, dy: int) -> bool | None:
         if self.start_game_rect.left <= x <= self.start_game_rect.right and \
@@ -140,3 +196,15 @@ class StartView(arcade.View):
             self.title_game_color = arcade.color.YELLOW_ORANGE
         else:
             self.title_game_color = arcade.color.WHITE
+
+        if self.free_game_rect.left <= x <= self.free_game_rect.right and \
+                self.free_game_rect.bottom <= y <= self.free_game_rect.top:
+            self.free_game_color = arcade.color.YELLOW_ORANGE
+        else:
+            self.free_game_color = arcade.color.WHITE
+
+        if self.back_game_rect.left <= x <= self.back_game_rect.right and \
+                self.back_game_rect.bottom <= y <= self.back_game_rect.top:
+            self.back_game_color = arcade.color.YELLOW_ORANGE
+        else:
+            self.back_game_color = arcade.color.WHITE
