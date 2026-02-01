e85a373f3106098f89ab55625821d37af4ef6c7a
dexon401 <@>
2026-02-01 15:28:42 +0300

    changed leaderboard.db structure and reworked db_manager.py

--- db_manager.py
+++ db_manager.py
@@ -5,15 +5,43 @@ class DBManager:
         self.con = sqlite3.connect('databases/leaderboard.db')
         self.cur = self.con.cursor()
         self.init_db()
+        self.tables = {
+            0: 'easy',
+            1: 'advanced',
+            2: 'hard'
+        }
 
     def init_db(self):
         self.cur.execute("""
-            CREATE TABLE IF NOT EXISTS score (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT UNIQUE NOT NULL,
-                score INTEGER DEFAULT 0
-            )
-        """)
+        CREATE TABLE IF NOT EXISTS easy (
+            id    INTEGER PRIMARY KEY AUTOINCREMENT
+                          NOT NULL
+                          UNIQUE,
+            name  TEXT    UNIQUE
+                          NOT NULL,
+            score INTEGER NOT NULL
+                          DEFAULT (0) 
+        )""")
+        self.cur.execute("""
+        CREATE TABLE IF NOT EXISTS advanced (
+            id    INTEGER PRIMARY KEY AUTOINCREMENT
+                          UNIQUE
+                          NOT NULL,
+            name  TEXT    NOT NULL
+                          UNIQUE,
+            score INTEGER NOT NULL
+                          DEFAULT (0) 
+        )""")
+        self.cur.execute("""
+        CREATE TABLE IF NOT EXISTS hard (
+            id    INTEGER PRIMARY KEY AUTOINCREMENT
+                          UNIQUE
+                          NOT NULL,
+            name  TEXT    UNIQUE
+                          NOT NULL,
+            score INTEGER NOT NULL
+                          DEFAULT (0) 
+        )""")
         self.con.commit()
 
     def cursor(self):
@@ -22,25 +50,29 @@ class DBManager:
     def connection(self):
         return self.con
 
-    def get_leaderboard(self):
-        leaderboard = self.con.cursor().execute("""
-        SELECT name, score FROM score
-        ORDER BY score DESC, name DESC""").fetchall()
+    def get_leaderboard(self, i):
+        leaderboard = self.con.cursor().execute(f"""
+        SELECT name, score FROM {self.tables[i]}
+        ORDER BY score DESC, name ASC""").fetchall()
         return leaderboard
 
-    def add_new_score(self, name: str, score: int):
-        score_id = self.cur.execute("""
-        SELECT id FROM score
+    def add_new_score(self, i, name: str, score: int):
+        table = self.tables[i]
+        score_id = self.cur.execute(f"""
+        SELECT id FROM {table}
         WHERE name = ?""", (name,)).fetchone()
 
         if score_id:
-            self.cur.execute("""
-            UPDATE score
-            SET score = ?
+            self.cur.execute(f"""
+            UPDATE {table}
+            SET score = MAX(score, ?)
             WHERE id = ?""", (score, score_id[0]))
         else:
-            self.cur.execute("""
-            INSERT INTO score(name, score)
+            self.cur.execute(f"""
+            INSERT INTO {table}(name, score)
             VALUES(?, ?)""", (name, score))
 
         self.con.commit()
+
+    def close(self):
+        self.con.close()
